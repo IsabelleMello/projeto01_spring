355e694864930715c90b57f329afbdf3c135c99a
IsabelleMello <isabelle.melo@dcx.ufpb.br>
2021-01-16 17:07:23 -0300

    Projeto criado

---
+++ src/main/java/ufpb/br/projeto_01/Projeto01Application.java
@@ -0,0 +1,13 @@
+package ufpb.br.projeto_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto01Application.class, args);
+	}
+
+}
---
+++ src/test/java/ufpb/br/projeto_01/Projeto01ApplicationTests.java
@@ -0,0 +1,13 @@
+package ufpb.br.projeto_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
